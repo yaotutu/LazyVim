69b4b0ec826801d2f6e0e3b1b1a0e89079b60e36
yaotutu <957810668@qq.com>
2024-07-20 15:13:39 +0800

    feat: Add Lua diagnostics globals for vim
    This commit adds the Lua diagnostics globals configuration for vim in the .vscode/settings.json file. This will help improve the code analysis and error checking for Lua scripts in the project.

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,57 @@
+local gstatus = { ahead = 0, behind = 0 }
+local function update_gstatus()
+    local Job = require("plenary.job")
+    Job:new({
+        command = "git",
+        args = { "rev-list", "--left-right", "--count", "HEAD...@{upstream}" },
+        on_exit = function(job, _)
+            local res = job:result()[1]
+            if type(res) ~= "string" then
+                gstatus = { ahead = 0, behind = 0 }
+                return
+            end
+            local ok, ahead, behind = pcall(string.match, res, "(%d+)%s*(%d+)")
+            if not ok then
+                ahead, behind = 0, 0
+            end
+            gstatus = { ahead = ahead, behind = behind }
+        end,
+    }):start()
+end
+
+if _G.Gstatus_timer == nil then
+    _G.Gstatus_timer = vim.loop.new_timer()
+else
+    _G.Gstatus_timer:stop()
+end
+_G.Gstatus_timer:start(0, 2000, vim.schedule_wrap(update_gstatus))
+
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+        { "dokwork/lualine-ex" },
+        { "nvim-lua/plenary.nvim" },
+        { "kyazdani42/nvim-web-devicons" },
+        "meuter/lualine-so-fancy.nvim",
+    },
+    event = "VeryLazy",
+    opts = function(_, opts)
+        opts.sections.lualine_b = {
+            "ex.git.branch",
+            {
+                function()
+                    return gstatus.ahead .. " " .. gstatus.behind .. ""
+                end,
+            },
+            "diff",
+            "diagnostics",
+            "fancy_macro",
+        }
+        opts.lualine_c = {
+            {
+                "lsp_progress",
+                spinner_symbols = { " ", " ", " ", " ", " ", " " },
+            },
+        }
+    end,
+}
