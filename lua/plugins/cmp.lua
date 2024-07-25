d89e958bf788a073a346cddae5b22e7cc3624f30
yaotutu <957810668@qq.com>
2024-07-25 09:12:19 +0800

    update

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,21 +1,54 @@
 return {
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-        local cmp = require("cmp")
-        print("Loading cmp custom mappings")
-        opts.mapping = cmp.mapping.preset.insert({
-            ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-            ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-            -- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-            -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-            ["<C-.>"] = cmp.mapping.complete(),
-            ["<C-b>"] = cmp.mapping.complete(),
-            ["<C-e>"] = cmp.mapping.abort(),
-            ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-            ["<S-CR>"] = cmp.mapping.confirm({
-                behavior = cmp.ConfirmBehavior.Replace,
-                select = true,
-            }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        })
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    print("Loading cmp custom mappings")
+    opts.mapping = cmp.mapping.preset.insert({
+      ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      -- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-.>"] = cmp.mapping.complete(),
+      ["<C-b>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ["<S-CR>"] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true,
+      }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    })
+    local has_words_before = function()
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
+
+    opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+          cmp.select_next_item()
+        elseif vim.snippet.active({ direction = 1 }) then
+          vim.schedule(function()
+            vim.snippet.jump(1)
+          end)
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif vim.snippet.active({ direction = -1 }) then
+          vim.schedule(function()
+            vim.snippet.jump(-1)
+          end)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    })
+  end,
 }
