b101e0a63663553fd6c3d391c2d13c2eda5cd0f8
yaotutu <957810668@qq.com>
2024-09-12 22:39:54 +0800

    update

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,12 +6,8 @@ return {
       ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
       ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
       ["<C-.>"] = cmp.mapping.complete(),
-      ["<C-b>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.abort(),
-      ["<S-CR>"] = cmp.mapping.confirm({
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = true,
-      }),
+      ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     })
   end,
 }
